974edcecae1378ca31438432d7c78648429b06cb
Cheerio <cheerio.the.bear@gmail.com>
2020-04-13 00:15:39 +0900

    Do test call from MainActivity
    This is just for a test which utilizes CardRepository from MainActivity.

--- app/src/main/java/com/github/cheeriotb/uiccbrowser/MainActivity.kt
+++ app/src/main/java/com/github/cheeriotb/uiccbrowser/MainActivity.kt
@@ -2,20 +2,18 @@ package com.github.cheeriotb.uiccbrowser
 
 import android.Manifest
 import android.content.pm.PackageManager
-import android.content.pm.Signature
-import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
-import androidx.annotation.RequiresApi
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.github.cheeriotb.uiccbrowser.cardio.Command
+import com.github.cheeriotb.uiccbrowser.cardio.Interface
 import com.github.cheeriotb.uiccbrowser.cardio.TelephonyInterface
+import com.github.cheeriotb.uiccbrowser.repository.CardRepository
 import com.github.cheeriotb.uiccbrowser.util.byteArrayToHexString
-import java.lang.Exception
-import java.security.MessageDigest
+import kotlinx.coroutines.runBlocking
 
 class MainActivity : AppCompatActivity() {
 
@@ -53,39 +51,44 @@ class MainActivity : AppCompatActivity() {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 
-    @RequiresApi(Build.VERSION_CODES.P)
     private fun execute() {
-        val packageInfo = packageManager.getPackageInfo(packageName,
-                PackageManager.GET_SIGNING_CERTIFICATES)
-        var signatures = packageInfo.signatures
-        val signingInfo = packageInfo.signingInfo
-        if (signingInfo != null) {
-            signatures = signingInfo.signingCertificateHistory
-            if (signingInfo.hasMultipleSigners()) {
-                signatures = signingInfo.apkContentsSigners
-            }
+        val repository = CardRepository.from(this, 0)
+        runBlocking {
+            repository!!.initialize()
+            repository.cacheFileControlParameters(fileId = "3F00")
+            repository.cacheFileControlParameters(fileId = "2F05")
+            repository.cacheFileControlParameters(
+                "A0000000871002FFFFFFFF8907090000",
+                "7FFF",
+                "5F3B"
+            )
+            repository.cacheFileControlParameters(
+                "A0000000871002FFFFFFFF8907090000",
+                "7FFF5F3B",
+                "4F20"
+            )
+            repository.cacheFileControlParameters(
+                "A0000000871002FFFFFFFF8907090000",
+                "7FFF5F3B",
+                "4F52"
+            )
+            val list0 = repository.queryFileControlParameters()
+            val list1 = repository.queryFileControlParameters(
+                "A0000000871002FFFFFFFF8907090000",
+                "7FFF"
+            )
+            val list2 = repository.queryFileControlParameters(
+                "A0000000871002FFFFFFFF8907090000",
+                "7FFF5F3B"
+            )
+            Log.d("UiccBrowser", "$list0")
+            Log.d("UiccBrowser", "$list1")
+            Log.d("UiccBrowser", "$list2")
+            repository.readBinary("A0000000871002FFFFFFFF8907090000", "7FFF", "6F46", 0)
+            repository.readRecord(CardRepository.AID_NONE, CardRepository.LEVEL_MF, "2F00", 1)
+            repository.readRecord(CardRepository.AID_NONE, CardRepository.LEVEL_MF, "2F00", 2)
+            repository.dispose()
         }
-        for (signature in signatures) {
-            Log.d("UiccBrowser", "signature = " + byteArrayToHexString(getCertHash(signature)))
-        }
-
-        val tif = TelephonyInterface.from(this, 0)
-        tif.openChannel(TelephonyInterface.BASIC_CHANNEL_AID)
-        val response = tif.transmit(Command(0xA4, 0x00, 0x04, byteArrayOf(b(0x3F), b(0x00))))
-        Log.d("UiccBrowser", "response.data = " + byteArrayToHexString(response.data))
-        Log.d("UiccBrowser", "response.sw = " + response.sw.toString(16))
-        Toast.makeText(this, response.sw.toString(16), Toast.LENGTH_SHORT).show()
-        tif.closeRemainingChannel()
-        tif.dispose()
-    }
-
-    private fun getCertHash(signature: Signature): ByteArray {
-        try {
-            val md = MessageDigest.getInstance("SHA-1")
-            return md.digest(signature.toByteArray())
-        } catch (e: Exception) {
-        }
-        return ByteArray(0)
     }
 
     private fun b(byte: Int) = byte.toByte()
