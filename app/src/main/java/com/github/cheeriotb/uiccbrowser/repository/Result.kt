dc3468c5f77c6a6e61d82ff40bf5d7c0efa62a50
Cheerio <cheerio.the.bear@gmail.com>
2020-04-18 11:17:17 +0900

    Introduce Builder for Result
    It might be better to apply Builder pattern for Result for improving
    readability of the code.

--- app/src/main/java/com/github/cheeriotb/uiccbrowser/repository/Result.kt
+++ app/src/main/java/com/github/cheeriotb/uiccbrowser/repository/Result.kt
@@ -10,7 +10,7 @@ package com.github.cheeriotb.uiccbrowser.repository
 
 import com.github.cheeriotb.uiccbrowser.cacheio.SelectResponse
 
-class Result(
+data class Result (
     val fileId: String,
     val data: ByteArray,
     val sw: Int
@@ -35,11 +35,56 @@ class Result(
             else -> false
         }
 
+    data class Builder(
+        private var fileId: String = "",
+        private var data: ByteArray = DATA_NONE,
+        private var sw: Int = SW_NORMAL
+    ) {
+        fun fileId(fileId: String) = apply { this.fileId = fileId }
+        fun data(data: ByteArray) = apply { this.data = data }
+        fun sw(sw: Int) = apply { this.sw = sw }
+        fun build() = Result(fileId, data, sw)
+    }
+
     companion object {
         fun listFrom(source: List<SelectResponse>): List<Result> {
             val destination: MutableList<Result> = mutableListOf()
-            source.forEach { destination.add(Result(it.fileId, it.data, it.sw)) }
+            source.forEach {
+                val result = Builder()
+                        .fileId(it.fileId)
+                        .data(it.data)
+                        .sw(it.sw)
+                        .build()
+                destination.add(result)
+            }
             return destination
         }
+
+        const val SW_NORMAL = 0x9000
+        const val SW_NOT_FOUND = 0x6A82
+
+        val DATA_NONE = ByteArray(0)
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Result
+
+        if (fileId != other.fileId) return false
+        if (!data.contentEquals(other.data)) return false
+        if (sw != other.sw) return false
+        if (isOk != other.isOk) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = fileId.hashCode()
+        result = 31 * result + data.contentHashCode()
+        result = 31 * result + sw
+        result = 31 * result + isOk.hashCode()
+        return result
     }
 }
